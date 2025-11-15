1ffdb5103147a44e4f58ca0489bffef16e3786ec
S3R43o3 <67480273+sera619@users.noreply.github.com>
2025-11-15 03:52:47 +0100

    add ROLE_RULE_ID

---
+++ views/rollbutton_view.py
@@ -0,0 +1,18 @@
+import disnake
+
+class RuleButtonView(disnake.ui.View):
+    def __init__(self, role_id: int):
+        super().__init__(timeout=None)
+        self.role_id = role_id
+    
+    @disnake.ui.button(label="📜 Regeln gelesen", style=disnake.ButtonStyle.green, custom_id="rulez_button")
+    async def role_button(self, button: disnake.ui.button, interaction: disnake.MessageInteraction):
+        guild = interaction.guild
+        member = interaction.user
+        role = guild.get_role(self.role_id)
+        
+        if role in member.roles:
+            await interaction.response.send_message("Du hast die Rolle bereits!", ephemeral=True, delete_after=7)
+        else:
+            await member.add_roles(role)
+            await interaction.response.send_message(f"Rolle **{role.name}** vergeben!", ephemeral=True, delete_after=7)
