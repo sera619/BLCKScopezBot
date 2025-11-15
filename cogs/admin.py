aed68ec0520b97d5ec02d663876bf9d560721f86
S3R43o3 <67480273+sera619@users.noreply.github.com>
2025-11-15 04:31:31 +0100

    fix typos

--- cogs/admin.py
+++ cogs/admin.py
@@ -9,25 +9,28 @@ class Admin(commands.Cog):
     @commands.is_owner()
     async def reload(self, ctx: commands.Context, cog: str):
         try:
+            await ctx.send(f"Start hot-reloading Extension: **{cog}**...", delete_after=7)
             self.bot.reload_extension(f"cogs.{cog}")
-            await ctx.send(f"Start hot-reload.\nHot-Reloaded **{cog}**", delete_after=7)
         except Exception as e:
             await ctx.send(f"Error hot-reloading {cog}:\n```{e}```", delete_after=7)
+    
             
     @commands.command(name="reloadall")
     @commands.has_permissions(administrator=True)
     @commands.is_owner()
     async def reload_all(self, ctx: commands.Context):
         reloaded = []
+        await ctx.send(f"Start hot-reloading ALL extensions...", delete_after=4)
         for ext in list(self.bot.extensions):
             try:
                 self.bot.reload_extension(ext)
                 reloaded.append(ext)
             except Exception as e:
                 await ctx.send(f"Error reload extension `{ext}`:\n```{e}```", delete_after=7)
-        await ctx.send(f"Hot-Reloaded:\n"+"\n".join(reloaded), delete_after=7)
+        await ctx.send(f"Successfully hot-reloaded:\n"+"\n".join(reloaded), delete_after=7)
 
-    # errohandling
+
+    # error handling
     @commands.Cog.listener()
     async def on_command_error(self, ctx: commands.Context, error):
         if isinstance(error, commands.CheckFailure):
