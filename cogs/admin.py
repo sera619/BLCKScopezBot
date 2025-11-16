ccc6337e0d385f8b7b19e081c053bfc6e40bdcc5
S3R43o3 <67480273+sera619@users.noreply.github.com>
2025-11-16 18:23:35 +0100

    init devpanel

--- cogs/admin.py
+++ cogs/admin.py
@@ -1,8 +1,11 @@
+import disnake
 from disnake.ext import commands
+from views.dev_cogmanager_view import CogManagerView
 
 class Admin(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
+        self.welcome_msg_filepath = "./data/welcome_message.md"
      
     @commands.command(name="reload")
     @commands.has_permissions(administrator=True)
@@ -30,7 +33,50 @@ class Admin(commands.Cog):
             except Exception as e:
                 await ctx.send(f"Error reload extension `{ext}`:\n```{e}```", delete_after=7)
         await ctx.send(f"Successfully hot-reloaded:\n"+"\n".join(reloaded), delete_after=7)
-
+        
+        
+    # devpanel
+    @commands.command(name="devpanel")
+    @commands.has_permissions(administrator=True)
+    @commands.is_owner()
+    async def devpanel(self, ctx: commands.Context):
+        embed = disnake.Embed(
+            title="Developer Control Panel",
+            description="Manage your bot extensions using the UI below.",
+            color=0x2ECC71
+        )
+        embed.add_field(
+            name="Available Cogs",
+            value="Dynamically loaded from /cogs/",
+            inline=False
+        )
+        
+        view = CogManagerView(self.bot)
+        await ctx.channel.send(embed=embed, view=view)
+    
+    # welcome message    
+    # load welcome message
+    def load_message(self) -> str:
+        try:
+            with open(self.welcome_msg_filepath, "r", encoding="utf-8") as f:
+                return f.read()
+        except Exception as e:
+            return f"Failed to load welcome message:\n```{e}```"
+        
+        
+    # post welcome message 
+    @commands.command(name="postwelcome")
+    @commands.has_permissions(administrator=True)
+    @commands.is_owner()
+    async def post_welcome(self, ctx: commands.Context):
+        await ctx.message.delete()
+        msg_content = self.load_message()
+        embed = disnake.Embed(
+            title=" ",
+            description=msg_content,
+            color=0x2ECC71
+        )
+        await ctx.send(embed=embed)
 
     # error handling
     @commands.Cog.listener()
@@ -41,7 +87,8 @@ class Admin(commands.Cog):
             pass
         else:
             await ctx.send(f"Ein Fehler ist aufgetreten:\n```{error}```")
-    
+
+
 def setup(bot: commands.Bot):
     bot.add_cog(Admin(bot))
                 
